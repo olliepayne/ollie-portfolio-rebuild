f8bf9bdd319d6b1448201ea604e2ac7a2bb6615d
olliepayne <olliepayne@me.com>
2021-03-30 15:35:38 -0700

    begin building portfolio page

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,8 +1,24 @@
+import portfolioData from '../../public/portfolioData.json'
+
 export const getStaticProps = async () => {
-  
+    return {
+      props: {
+        categories: portfolioData.filters.categories,
+        tags: portfolioData.filters.tags,
+        entries: portfolioData.entries
+      }
+    }
 }
 
-const Portfolio = () => {
+const EntryCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+const Portfolio = ({ categories, tags, entries }) => {
   return (
     <div className="page-card">
       <h1>Portfolio</h1>
