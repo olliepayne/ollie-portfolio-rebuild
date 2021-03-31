846656df4e603bdca762b9b7de1f5c0c29e37aba
olliepayne <olliepayne@me.com>
2021-03-30 18:22:26 -0700

    backup

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import styles from '../../styles/Portfolio.module.css'
 import portfolioData from '../../public/portfolioData.json'
 
 export const getStaticProps = async () => {
-    return {
-      props: {
-        filters: portfolioData.filters,
-        entries: portfolioData.entries
-      }
+  return {
+    props: {
+      filters: portfolioData.filters,
+      entries: portfolioData.entries
     }
+  }
 }
 
 const EntryCard = () => {
@@ -19,27 +20,49 @@ const EntryCard = () => {
 }
 
 const Portfolio = ({ filters, entries }) => {
-  const [appliedFilters, setAppliedFilters] = useState([String])
+  const [appliedFilters, setAppliedFilters] = useState([])
   const [filteredEntries, setFilteredEntries] = useState(entries)
 
-  const applyFilter = () => {
-    let entriesCopy = entries.slice()
+  const filterContent = (newFilters: string[]) => {
+    let entriesCopy: typeof entries[] = entries.slice()
     entriesCopy = entriesCopy.filter(entry => {
-     appliedFilters.forEach(filter => {
-       if(entry.filters.categories.includes(filter) || entry.filters.tags.includes(filter)) return entry
-     })
+      let isMatch: boolean = false
+      newFilters.forEach(filter => {
+        if (entry.filters.categories.includes(filter) || entry.filters.tags.includes(filter)) return (isMatch = true)
+      })
+      console.log(isMatch)
     })
     console.log(entriesCopy)
   }
 
+  const applyFilter = e => {
+    e.preventDefault()
+
+    // NEWDATA IS UNDEFINED
+    const newData: string[] = appliedFilters.slice()
+    newData.push(e.target.value)
+    console.log(newData)
+    setAppliedFilters(newData)
+
+    filterContent(newData)
+  }
+
   return (
     <div className="page-card">
-      <h1>Portfolio</h1>
-      <ul>
-        {filteredEntries.map(entry => (
-          <li>{entry.title}</li>
-        ))}
-      </ul>
+      <div className={styles.content}>
+        <div className={styles.firstRow}>
+          <h1>Portfolio</h1>
+          <form className={styles.filterForm} onSubmit={applyFilter}>
+            <input className={styles.filterInput} name="filter" type="text" placeholder="Enter filter..." />
+            <button className={styles.applyButton}>Apply</button>
+          </form>
+        </div>
+        <ul>
+          {filteredEntries.map(entry => (
+            <li key={entry.id}>{entry.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
