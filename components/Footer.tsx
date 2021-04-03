851afbbc84bc21c152733a9e4b9b5520c93346e0
olliepayne <olliepayne@me.com>
2021-04-02 17:50:58 -0700

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,22 @@
 const Footer = () => {
+  return (
+    <footer>
+      <style jsx>
+        {`
+          footer {
+            font-style: italic;
+            text-align: center;
+          }
 
+          .github-link {
+            color: cornflowerblue;
+            font-weight: bold;
+          }
+        `}
+      </style>
+      <p>Made with Next.js, checkout the <a className="github-link" href="/https://github.com/olliepayne/ollie-portfolio-rebuild" target="blank">Github repo</a></p>
+    </footer>
+  )
 }
 
 export default Footer
