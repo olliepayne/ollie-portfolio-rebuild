c950c2903d55fb0406c994b57a493e5b05b58926
olliepayne <olliepayne@me.com>
2021-04-05 09:34:37 -0700

    introduce responsive component rendering

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react'
 import Navbar from './Navbar'
+import MobileNavbar from './MobileNavbar'
 import Footer from './Footer'
 
 const Layout = ({ children }) => {
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
+
+  const handleResize = () => {
+    const newData = Object.assign({}, dimensions)
+    newData.width = window.innerWidth
+    newData.height = window.innerHeight
+    setDimensions(newData)
+  }
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <>
-      <Navbar />
+      {dimensions.width > 480 ? <Navbar /> : <MobileNavbar />}
       {children}
       <Footer />
     </>
