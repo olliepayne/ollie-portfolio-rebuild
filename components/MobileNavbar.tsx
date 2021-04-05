151c73a9860bedc9bbf737b93f26f33203a82da2
olliepayne <olliepayne@me.com>
2021-04-05 09:48:35 -0700

    backup

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -1,16 +1,27 @@
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
-import styles from '../styles/Navbar.module.css'
+import Image from 'next/image'
+import styles from '../styles/MobileNavbar.module.css'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.title}>
+      <div className={styles.top}>
         <Link href="/"><a><h1>Ollie Payne Mobile</h1></a></Link>
+        <button onClick={handleOpen}>{isOpen ? '[collapse]' : '[expand]' }</button>
       </div>
-      <ul className={styles.navLinksList}>
-        <li><Link href="/portfolio"><a className={styles.navLink}>Portfolio</a></Link></li>
-        <li><Link href="/contact"><a className={styles.navLink}>Contact</a></Link></li>
-      </ul>
+      {isOpen &&
+        <ul className={styles.navLinksList}>
+          <li><Link href="/portfolio"><a className={styles.navLink}>Portfolio</a></Link></li>
+          <li><Link href="/contact"><a className={styles.navLink}>Contact</a></Link></li>
+        </ul>
+      }
     </nav>
   )
 }
