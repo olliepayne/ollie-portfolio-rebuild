befbb1d372b7369d01e2a013c5d938f7197b637e
olliepayne <olliepayne@me.com>
2021-06-15 16:48:48 -0700

    stub nav and begin testing theme ui

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,66 @@
+/** @jsxImportSource theme-ui */
+import Link from 'next/link'
+
+const NavHeader = () => {
+  return (
+    <div
+      sx={{
+        textAlign: 'center'
+      }}
+    >
+      <h1>Ollie Payne</h1>
+    </div>
+  )
+}
+
+const NavLink = ({ targetUrl, text }) => {
+  return (
+    <div
+      sx={{
+        height: '45%',
+        padding: '0.5em',
+        margin: '0 1em',
+        ':hover': { cursor: 'pointer', backgroundColor: 'lightgray' }
+      }}
+    >
+      <Link href={targetUrl}>
+        <a
+          sx={{
+
+          }}
+        >
+          {text}
+        </a>
+      </Link>
+    </div>
+  )
+}
+
+const NavLinkList = () => {
+  return (
+    <div
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+      }}
+    >
+      <NavLink targetUrl="/portfolio" text="Portfolio" />
+      <NavLink targetUrl="/contact" text="Contact" />
+    </div>
+  )
+}
+
 const Nav = () => {
   return (
-    <nav>
-
+    <nav
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-around'
+      }}
+    >
+      <NavHeader />
+      <NavLinkList />
     </nav>
   )
 }
